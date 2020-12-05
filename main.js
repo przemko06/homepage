20010a4f81e6e12b1475f4f45c9a3b74a0da4368
Przemyslaw Szlachta <szlachta.przemek@gmail.com>
2020-12-05 18:02:25 +0100

    querySelector & inneHTML testing

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ const age = 29;
 console.log(firstName);
 console.log(age);
 console.log(`Siema, nazywam się ${firstName} i mam ${age} lat`);
+
+const heading = document.querySelector(".main-heading--js");
+
+heading.innerHTML = `Witaj drogi odwiedzający, nazywam się ${firstName} i mam ${age} lat`;
+
+console.log(heading);
